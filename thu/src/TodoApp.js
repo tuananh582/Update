a11799c2c92a98e0e5590b037df386ba7c04d3a0
tuananh582 <phamtuananh582@gmail.com>
2023-05-17 20:25:03 +0700

    baicuoikhoa

--- thu/src/TodoApp.js
+++ thu/src/TodoApp.js
@@ -114,30 +114,7 @@ const TodoApp = () =>  {
         </ul>
       </div>
 
-      {/* <div className="filter-container">
-        <button
-          className={filter === 'all' ? 'active' : ''}
-          onClick={() => setFilter('all')}
-        >
-          All
-        </button>
-        <button
-          className={filter === 'active' ? 'active' : ''}
-          onClick={() => setFilter('active')}
-        >
-          Active
-        </button>
-        <button
-          className={filter === 'completed' ? 'active' : ''}
-          onClick={() => setFilter('completed')}
-        >
-          Completed
-        </button>
-      </div>
-
-      <button className="clear-button" onClick={() => setTasks([])}>
-        Clear All
-      </button> */}
+     
     </div>
   );
 }
